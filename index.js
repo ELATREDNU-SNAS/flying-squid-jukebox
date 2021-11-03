27f16e5efda88e51ed321c574a05f64a0bb925f1
ELATREDNU-SNAS <70924774+ELATREDNU-SNAS@users.noreply.github.com>
2021-11-03 21:42:33 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const nbsReader = require ("nbs-reader");
 
-module.exports.player = (player, serv) => {
-    player.opened = nbsReader('necron.nbs')
-    console.log(player.opened.Notes);
-player.beginningtick = serv.tickCount
+module.exports.server = (serv) => {
+  console.log(this);
+    opened = nbsReader('example.nbs')
+    console.log(opened.Notes);
+beginningtick = 0
 
 instruments = [
   'note.harp', //Piano (Air)
@@ -11,30 +12,37 @@ instruments = [
   'note.bd', //Bass Drum (Stone)
   'note.snare', //Snare Drum (Sand)
   'note.hat', //Click (Glass)
-  'note.guitar', //Guitar (Wool)                       //DOESN'T PLAY 1.8.9
-  'note.flute', //Flute (Clay)                         //DOESN'T PLAY 1.8.9
-  'note.bell', //Bell (Block of Gold)                  //DOESN'T PLAY 1.8.9
-  'note.chime', //Chime (Packed Ice)                   //DOESN'T PLAY 1.8.9
-  'note.xylophone', //Xylophone (Bone Block)           //DOESN'T PLAY 1.8.9
-  'note.iron_xylophone', //Iron Xylophone (Iron Block) //DOESN'T PLAY 1.8.9
-  'note.cow_bell', //Cow Bell (Soul Sand)              //DOESN'T PLAY 1.8.9
-  'note.didgeridoo', //Didgeridoo (Pumpkin)            //DOESN'T PLAY 1.8.9
-  'note.bit', //Bit (Block of Emerald)                 //DOESN'T PLAY 1.8.9
-  'note.banjo', //Banjo (Hay)                          //DOESN'T PLAY 1.8.9
+  'note.guitar', //Guitar (Wool)                   
+  'note.flute', //Flute (Clay)                       
+  'note.bell', //Bell (Block of Gold)                 
+  'note.chime', //Chime (Packed Ice)                   
+  'note.xylophone', //Xylophone (Bone Block)           
+  'note.iron_xylophone', //Iron Xylophone (Iron Block) 
+  'note.cow_bell', //Cow Bell (Soul Sand)             
+  'note.didgeridoo', //Didgeridoo (Pumpkin)            
+  'note.bit', //Bit (Block of Emerald)              
+  'note.banjo', //Banjo (Hay)                         
   'note.pling', //Pling (Glowstone)
 ]
 
+tempo = Math.ceil(opened.Tempo/20)
+
 serv.on('tick', (e) => {
-tempo = Math.ceil(player.opened.Tempo/20)
-console.log(tempo);
-player.opened.Notes.forEach((item, i) => {
-  if ((i*tempo) == Math.floor((serv.tickCount - player.beginningtick)) ) {
-    //console.log(i);
+
+
+opened.Notes.forEach((item, i) => {
+  if ((i*tempo) == Math.floor((serv.tickCount - beginningtick)) ) {
+
     item.forEach((inst, t) => {
 if (inst.Inst <= instruments.length) {
 
         pitchNew = 63 * Math.pow(2,((((inst.Key - 33) + (inst.Pitch / 100)) - 12) / 12))
-        
+if (pitchNew > 255) {
+  pitchNew = 255
+}
+
+serv.players.forEach((player, i) => {
+
         player._client.write('named_sound_effect', {
                  soundName: instruments[inst.Inst],
                  soundCategory: 0,
@@ -44,13 +52,15 @@ if (inst.Inst <= instruments.length) {
                  volume: inst.Velocity,
                  pitch:  pitchNew//63
                })
+
+               });
 }
 
 
     });
 
-    if (i == player.opened.Length) {
-      player.beginningtick = serv.tickCount
+    if (i == opened.Length) {
+      beginningtick = serv.tickCount
     }
 
   }
