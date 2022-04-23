1c7fc65130eb105cb68f61d89fb01b31ab88c019
ELATREDNU-SNAS <70924774+ELATREDNU-SNAS@users.noreply.github.com>
2022-04-23 21:35:57 +1000

    Adjusted to become class object
    need to test, may not function as intended.

--- index.js
+++ index.js
@@ -1,74 +1,83 @@
 const nbsReader = require ("nbs-reader");
 
 module.exports.server = (serv) => {
-  console.log(this);
-    opened = nbsReader('example.nbs')
-    console.log(opened.Notes);
-beginningtick = 0
 
-instruments = [
+class Song {
+  constructor(file) {
+this.currentSong = nbsReader(file)
+
+this.instruments = [
   'note.harp', //Piano (Air)
   'note.bass', //Double Bass (Wood)
   'note.bd', //Bass Drum (Stone)
   'note.snare', //Snare Drum (Sand)
   'note.hat', //Click (Glass)
-  'note.guitar', //Guitar (Wool)                   
-  'note.flute', //Flute (Clay)                       
-  'note.bell', //Bell (Block of Gold)                 
-  'note.chime', //Chime (Packed Ice)                   
-  'note.xylophone', //Xylophone (Bone Block)           
-  'note.iron_xylophone', //Iron Xylophone (Iron Block) 
-  'note.cow_bell', //Cow Bell (Soul Sand)             
-  'note.didgeridoo', //Didgeridoo (Pumpkin)            
-  'note.bit', //Bit (Block of Emerald)              
-  'note.banjo', //Banjo (Hay)                         
+  'note.guitar', //Guitar (Wool)
+  'note.flute', //Flute (Clay)
+  'note.bell', //Bell (Block of Gold)
+  'note.chime', //Chime (Packed Ice)
+  'note.xylophone', //Xylophone (Bone Block)
+  'note.iron_xylophone', //Iron Xylophone (Iron Block)
+  'note.cow_bell', //Cow Bell (Soul Sand)
+  'note.didgeridoo', //Didgeridoo (Pumpkin)
+  'note.bit', //Bit (Block of Emerald)
+  'note.banjo', //Banjo (Hay)
   'note.pling', //Pling (Glowstone)
 ]
 
-tempo = Math.ceil(opened.Tempo/20)
+this.tempo = Math.ceil(this.currentSong.Tempo/20)
 
-serv.on('tick', (e) => {
+this.currentTick = 0
+  }
 
+  play() {
+    serv.on("tick", (e) => {
+  this.currentSong.Notes.forEach((item, i) => {
+    if (i * this.tempo == Math.floor(serv.tickCount - this.currentTick)) {
+      item.forEach((inst, t) => {
+        if (inst.Inst <= this.instruments.length) {
+          currentPitch = 63 * Math.pow(2, (inst.Key - 33 + inst.Pitch / 100 - 12) / 12);
+          if (currentPitch > 255) {
+            currentPitch = 255;
+          }
 
-opened.Notes.forEach((item, i) => {
-  if ((i*tempo) == Math.floor((serv.tickCount - beginningtick)) ) {
+          serv.players.forEach((player, i) => {
+            player._client.write("named_sound_effect", {
+              soundName: this.instruments[inst.Inst],
+              soundCategory: 0,
+              x: player.position.x,
+              y: player.position.y + 1,
+              z: player.position.z,
+              volume: inst.Velocity,
+              pitch: currentPitch,
+            });
+          });
+        }
+      });
 
-    item.forEach((inst, t) => {
-if (inst.Inst <= instruments.length) {
-
-        pitchNew = 63 * Math.pow(2,((((inst.Key - 33) + (inst.Pitch / 100)) - 12) / 12))
-if (pitchNew > 255) {
-  pitchNew = 255
-}
-
-serv.players.forEach((player, i) => {
-
-        player._client.write('named_sound_effect', {
-                 soundName: instruments[inst.Inst],
-                 soundCategory: 0,
-                 x: player.position.x,
-                 y: player.position.y + 1,
-                 z: player.position.z,
-                 volume: inst.Velocity,
-                 pitch:  pitchNew//63
-               })
-
-               });
-}
-
-
-    });
-
-    if (i == opened.Length) {
-      beginningtick = serv.tickCount
+      if (i == this.currentSong.Length) {
+        this.currentTick = serv.tickCount;
+      }
     }
+  });
+});
 
   }
 
-});
+  pause() {
+    serv.on("tick", (e) => {
+      this.currentTick = this.currentTick - 1
+      if (this.currentTick < 0) {
+        this.currentTick = 0
+      }
+    });
+  }
+
+}
 
 
-})
+//EXAMPLE: exampleSong = new Song("example.nbs");
+
 
 
 }
